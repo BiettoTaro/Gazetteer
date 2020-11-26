fd0947e812e14896e584ab3083339fe2c08f5df9
Bietto <fabios90@icloud.com>
2020-11-26 23:36:12 +0000

    fix position and bounds

--- libs/js/script.js
+++ libs/js/script.js
@@ -29,17 +29,51 @@ window.onclick = function (event) {
   };
 };
 
-const gotPos = position => {
-  let lat = position.coords.latitude;
-  let lng = position.coords.longitude;
+let map = L.map('map').setView([51.505, -0.09], 13);
 
-  const map = L.map('map').setView([lat, lng], 13);
+
+const gotPos =  position => {
+  let lat =  position.coords.latitude;
+  let lng =  position.coords.longitude;
+
+ $(window).ready(function () {
+  $('#loader').fadeOut('slow');
+
+   $.ajax({
+    url: 'libs/php/initPosition.php',
+    type: 'POST',
+    dataType: 'json',
+    data: {
+      lat: lat,
+      lng: lng
+    },
+    success: function (result) {
+console.log(result)
+
+
+      if (result.status.name == 'ok') {
+        $('#countrySelect').val(result['data']['countryCode']);
+        let markers = new L.FeatureGroup();
+        map.addLayer(markers);
+
+          const marker = L.marker([lat, lng]).addTo(markers);
+
+          map.fitBounds(markers.getBounds());
+
+      }
+    }
+  });
+ });
+
+  // L.geoJSON(position).addTo(map);
+
   const OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
+    maxZoom: 15,
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
-  const marker = L.marker([lat, lng]).addTo(map);
-}
+
+};
+
 
 const fail = err => {
   let errors = {
@@ -48,30 +82,29 @@ const fail = err => {
     3: 'took too long'
   };
   console.log(errors[err]);
-}
+};
 
+if (navigator.geolocation) {
+  let giveUp = 1000 * 30;
+  let tooOld = 1000 * 60 * 5;
+  let options = {
+    enableHighAccuracy: true,
+    timeout: giveUp,
+    maximumAge: tooOld
+  }
+  navigator.geolocation.getCurrentPosition(gotPos, fail, options);
+} else {
 
+};
 
 
 $(document).ready(function () {
-  
 
-  if (navigator.geolocation) {
-    let giveUp = 1000 * 30;
-    let tooOld = 1000 * 60 * 5;
-    let options = {
-      enableHighAccuracy: true,
-      timeout: giveUp,
-      maximumAge: tooOld
-    }
-    navigator.geolocation.getCurrentPosition(gotPos, fail, options);
-  } else {
-    // hard coded coordinates?
-  };
 
-  $(window).load(function () {
-    // Animate loader off screen
-    $('#loader').style.display='none';
+
+
+  $(window).ready(function () {
+
 
     $.ajax({
       url: 'libs/php/countryBorder.php',
@@ -85,6 +118,7 @@ $(document).ready(function () {
         $('#countrySelect').html('');
 
         $.each(result.data, function (index) {
+          
           $('#countrySelect').append($('<option>', {
             value: result.data[index].code,
             text: result.data[index].name
@@ -98,73 +132,104 @@ $(document).ready(function () {
         console.log(jqXHR);
       }
     });
+  });
 
-    $('#myBtn').click(function () {
+  $('#countrySelect').change(function () {
+    $.ajax({
+      url: 'libs/php/borders.php',
+      type: 'POST',
+      dataType: 'json',
+      data: {
+        country: $('#countrySelect').val()
 
-      $.ajax({
-        url: 'libs/php/getCountryInfo.php',
-        type: 'POST',
-        dataType: 'json',
-        data: {
-          country: $('#countrySelect').val()
-        },
-        success: function (result) {
+      },
+      success: function (result) {
+
+
+        const myLayer = L.geoJSON().addTo(map);
+        if (result.status.name == 'ok') {
+
+        let newLayer = myLayer.addData(result['data']);
+        map.fitBounds(newLayer.getBounds());
+        }
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        // your error code
+        console.log(errorThrown);
+        console.log(textStatus);
+        console.log(jqXHR);
+      }
+    });
+  })
+
+
+
+  $('#myBtn').click(function () {
+
+    $.ajax({
+      url: 'libs/php/getCountryInfo.php',
+      type: 'POST',
+      dataType: 'json',
+      data: {
+        country: $('#countrySelect').val()
+      },
+      success: function (result) {
         console.log(result);
-          if (result.status.name == 'ok') {
-            $('#flag').attr('src', `https://www.countryflags.io/${$('#countrySelect').val()}/flat/64.png`);
-            $('#txtCountry').html(result['data'][0]['countryName']);
-            $('#txtCapital').html(result['data'][0]['capital']);
-            $('#txtPop').html(roundData(result['data'][0]['population']));
-            $('#txtCurrency').html(result['data'][0]['currencyCode']);
-            $('#wiki').attr('href', `https://en.wikipedia.org/wiki/${$('#countrySelect option:selected').text()}`);
+        if (result.status.name == 'ok') {
+          $('#flag').attr('src', `https://www.countryflags.io/${$('#countrySelect').val()}/flat/64.png`);
+          $('#txtCountry').html(result['data'][0]['countryName']);
+          $('#txtCapital').html(result['data'][0]['capital']);
+          $('#txtPop').html(roundData(result['data'][0]['population']));
+          $('#txtCurrency').html(result['data'][0]['currencyCode']);
+          $('#wiki').attr('href', `https://en.wikipedia.org/wiki/${$('#countrySelect option:selected').text()}`);
 
-            
-          }
+
         }
-      })
-    });
+      }
+    })
+  });
 
-    $('#myBtn').click(function () {
-      $.ajax({
-        url: 'libs/php/getWeather.php',
-        type: 'POST',
-        dataType: 'json',
-        data: {
-          city: $('#countrySelect option:selected').text()
-        },
-        success: function (result) {
+  $('#myBtn').click(function () {
+    $.ajax({
+      url: 'libs/php/getWeather.php',
+      type: 'POST',
+      dataType: 'json',
+      data: {
+        city: $('#countrySelect option:selected').text()
+      },
+      success: function (result) {
 
 
-          if (result.status.name == 'ok') {
-            $('#txtTemp').html(result['data']['main']['temp'] + '°C');
-            $('#txtWeather').html(result['data']['weather'][0]['description']);
-          }
+        if (result.status.name == 'ok') {
+          $('#txtTemp').html(result['data']['main']['temp'] + '°C');
+          $('#txtWeather').html(result['data']['weather'][0]['description']);
         }
-      });
-    });
-
-    $('#myBtn').click(function () {
-      $.ajax({
-        url: 'libs/php/covid.php',
-        type: 'POST',
-        dataType: 'json',
-        data: {
-          country: $('#countrySelect option:selected').text()
-        },
-        success: function (result) {
-
-          let data = result['data'];
-          let covid = data.pop();
-
-          if (result.status.name == 'ok') {
-            $('#txtCases').html(roundData(covid['Confirmed']));
-            $('#txtDeaths').html(roundData(covid['Deaths']));
-          }
-        }
-      });
+      }
     });
 
 
+
+  });
+
+  $('#myBtn').click(function () {
+    $.ajax({
+      url: 'libs/php/covid.php',
+      type: 'POST',
+      dataType: 'json',
+      data: {
+        country: $('#countrySelect option:selected').text()
+      },
+      success: function (result) {
+
+        let data = result['data'];
+        let covid = data.pop();
+
+        if (result.status.name == 'ok') {
+          $('#txtCases').html(roundData(covid['Confirmed']));
+          $('#txtDeaths').html(roundData(covid['Deaths']));
+        }
+      }
+    });
   });
 });
 
