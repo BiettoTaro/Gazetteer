7056680e198a779c643cd0e0dcace9c164f541f1
Bietto <fabios90@icloud.com>
2020-11-29 21:35:38 +0000

    add easybutton and new data

--- libs/js/script.js
+++ libs/js/script.js
@@ -1,8 +1,18 @@
 // modal
-const modal = document.getElementById("myModal");
+const details = document.getElementById("myModal");
+const virus = document.getElementById("virus");
+const news = document.getElementById("news");
+const weather = document.getElementById("weather");
+const images = document.getElementById("images");
+const currency = document.getElementById("currency");
+
+
 const btn = document.getElementById("myBtn");
-const span = document.getElementsByClassName("close")[0];
+const span = document.getElementsByClassName("close");
+
 let border;
+
+
 const roundData = data => {
   let num = Math.abs(Number(data));
 
@@ -15,21 +25,46 @@ const roundData = data => {
 
 };
 
+const countryInfo = () => {
+  $.ajax({
+    url: 'libs/php/getCountryInfo.php',
+    type: 'POST',
+    dataType: 'json',
+    data: {
+      country: $('#countrySelect').val()
+    },
+    success: function (result) {
+
+      if (result.status.name == 'ok') {
+        $('#flag').attr('src', `https://www.countryflags.io/${$('#countrySelect').val()}/flat/64.png`);
+        $('#txtCountry').html(result.data[0].countryName);
+        $('#txtCapital').html(result.data[0].capital);
+        $('#txtPop').html(roundData(result.data[0].population));
+        $('#txtCurrency').html(result.data[0].currencyCode);
+        $('#wiki').attr('href', `https://en.wikipedia.org/wiki/${$('#countrySelect option:selected').text()}`);
+      }
+    }
+  })
+}
+
 btn.onclick = function () {
-  modal.style.display = "block";
+  details.style.display = "block";
 };
 
-span.onclick = function () {
-  modal.style.display = "none";
-};
+$('.close').click(function () {
+  details.style.display = "none";
+});
 
 window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == details) {
+    details.style.display = "none";
   };
 };
 
 let map = L.map('map').setView([51.505, -0.09], 13);
+
+
+
 
 
 const gotPos = position => {
@@ -50,9 +85,8 @@ const gotPos = position => {
       success: function (result) {
 
 
-
         if (result.status.name == 'ok') {
-          $('#countrySelect').val(result['data']['countryCode']);
+          $('#countrySelect').val(result.data.countryCode);
           let markers = new L.FeatureGroup();
           map.addLayer(markers);
 
@@ -62,7 +96,7 @@ const gotPos = position => {
 
         }
       }
-    });
+    }).then(countryInfo)
   });
 
   // L.geoJSON(position).addTo(map);
@@ -150,12 +184,12 @@ $(document).ready(function () {
         if (result.status.name == 'ok') {
           if (map.hasLayer(border)) {
             map.removeLayer(border);
-        }
-           border = myLayer.addData(result['data'], {
+          }
+          border = myLayer.addData(result.data, {
             color: '#ff7800',
             weight: 2,
             opacity: 0.65
-           });
+          });
           map.fitBounds(border.getBounds());
         }
       },
@@ -165,37 +199,96 @@ $(document).ready(function () {
         console.log(textStatus);
         console.log(jqXHR);
       }
-    });
+    }).then(countryInfo);
   })
 
+  //covid easybutton
+  L.easyButton('fa-virus', function () {
+    virus.style.display = "block";
 
+    $('.close').click(function () {
+      virus.style.display = "none";
+    });
 
-  $('#myBtn').click(function () {
+    window.onclick = function (event) {
+      if (event.target == virus) {
+        virus.style.display = "none";
+      };
+    };
 
     $.ajax({
-      url: 'libs/php/getCountryInfo.php',
+      url: 'libs/php/covid.php',
+      type: 'POST',
+      dataType: 'json',
+      data: {
+        country: $('#countrySelect option:selected').text()
+      },
+      success: function (result) {
+
+        let data = result.data;
+        let covid = data.pop();
+
+        if (result.status.name == 'ok') {
+          $('#txtConfirmed').html(roundData(covid.Confirmed));
+          $('#txtActive').html(roundData(covid.Active));
+          $('#txtRecovered').html(roundData(covid.Recovered));
+          $('#txtDeaths').html(roundData(covid.Deaths));
+        }
+      }
+    });
+  }).addTo(map);
+
+  L.easyButton('fa-newspaper', function () {
+    news.style.display = "block";
+
+    $('.close').click(function () {
+      news.style.display = "none";
+    });
+
+    window.onclick = function (event) {
+      if (event.target == news) {
+        news.style.display = "none";
+      };
+    };
+
+    $.ajax({
+      url: 'libs/php/news.php',
       type: 'POST',
       dataType: 'json',
       data: {
         country: $('#countrySelect').val()
       },
       success: function (result) {
-        console.log(result);
-        if (result.status.name == 'ok') {
-          $('#flag').attr('src', `https://www.countryflags.io/${$('#countrySelect').val()}/flat/64.png`);
-          $('#txtCountry').html(result['data'][0]['countryName']);
-          $('#txtCapital').html(result['data'][0]['capital']);
-          $('#txtPop').html(roundData(result['data'][0]['population']));
-          $('#txtCurrency').html(result['data'][0]['currencyCode']);
-          $('#wiki').attr('href', `https://en.wikipedia.org/wiki/${$('#countrySelect option:selected').text()}`);
 
+
+
+        const data = result.data.articles[0];
+
+
+        if (result.status.name == 'ok') {
+          $('#newsImg').attr('src', data.urlToImage);
+          $('#txtTitle').html(data.title);
+          $('#txtDescription').html(data.description);
+          $('#newsLink').attr('href', data.url);
 
         }
       }
-    })
-  });
+    });
+  }).addTo(map);
 
-  $('#myBtn').click(function () {
+  L.easyButton('fa-cloud-sun-rain', function () {
+    weather.style.display = "block";
+
+    $('.close').click(function () {
+      weather.style.display = "none";
+    });
+
+    window.onclick = function (event) {
+      if (event.target == weather) {
+        weather.style.display = "none";
+      };
+    };
+
     $.ajax({
       url: 'libs/php/getWeather.php',
       type: 'POST',
@@ -206,37 +299,93 @@ $(document).ready(function () {
       success: function (result) {
 
 
+
+        const icon = result.data.weather[0].icon;
+
         if (result.status.name == 'ok') {
-          $('#txtTemp').html(result['data']['main']['temp'] + '°C');
-          $('#txtWeather').html(result['data']['weather'][0]['description']);
+          $('#weatherIcon').attr('src', `https://openweathermap.org/img/wn/${icon}.png`);
+          $('#txtWeather').html(result.data.weather[0].description);
+          $('#txtTemp').html(result.data.main.temp + '°C');
+          $('#tempMax').html(result.data.main.temp_max + '°C');
+          $('#tempMin').html(result.data.main.temp_min + '°C');
+          $('#tempFeels').html(result.data.main.feels_like + '°C');
         }
       }
     });
+  }).addTo(map);
 
 
+  L.easyButton('fa-image', function () {
+    images.style.display = "block";
 
-  });
+    $('.close').click(function () {
+      images.style.display = "none";
+    });
 
-  $('#myBtn').click(function () {
+    window.onclick = function (event) {
+      if (event.target == images) {
+        images.style.display = "none";
+      };
+    };
+
+
     $.ajax({
-      url: 'libs/php/covid.php',
+      url: 'libs/php/images.php',
       type: 'POST',
       dataType: 'json',
       data: {
-        country: $('#countrySelect option:selected').text()
+        image: $('#txtCapital').text()
       },
       success: function (result) {
 
-        let data = result['data'];
-        let covid = data.pop();
-
         if (result.status.name == 'ok') {
-          $('#txtCases').html(roundData(covid['Confirmed']));
-          $('#txtDeaths').html(roundData(covid['Deaths']));
+          $('#img1').attr('src', result.data.results[0].urls.regular);
+          $('#img2').attr('src', result.data.results[1].urls.regular);
+          $('#img3').attr('src', result.data.results[2].urls.regular);
+
         }
       }
     });
-  });
+
+  }).addTo(map);
+
+  L.easyButton('fa-pound-sign', function () {
+    currency.style.display = "block";
+
+    $('.close').click(function () {
+      currency.style.display = "none";
+    });
+
+    window.onclick = function (event) {
+      if (event.target == currency) {
+        currency.style.display = "none";
+      };
+    };
+
+    $('#convertBtn').click(function () {
+      $.ajax({
+        url: 'libs/php/currency.php',
+        type: 'POST',
+        dataType: 'json',
+        data: {
+          currency: $('#txtCurrency').text(),
+          amount: $('#converter').val()
+        },
+        success: function (result) {
+
+          if (result.status.name == 'ok') {
+
+            $('#result').html(result.data.amount + ` ${$('#txtCurrency').text()}`)
+
+
+          }
+        }
+      });
+    })
+
+
+
+  }).addTo(map);
+
+
 });
-
-
